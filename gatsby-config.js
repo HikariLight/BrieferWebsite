76ab51871d271e72d044438a982c5d586e33b3d7
Dhia M <merzougui.dhia@gmail.com>
2022-03-07 10:33:59 +0100

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
       title: `clearview`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms"]
+  plugins: ["gatsby-plugin-netlify-cms",
+  {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      name: "Briefer",
+      short_name: "Briefer",
+      start_url: "/",
+      background_color: "#EBF2FA",
+      theme_color: "#364954",
+      display: "standalone",
+      icon: "static/logo.png"
+    }
+  }
+],
+  
 };
