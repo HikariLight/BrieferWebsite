7e178f9e8cbec36970460cb8256f086878e55707
Dhia M <merzougui.dhia@gmail.com>
2022-03-24 16:35:01 +0100

    Updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,8 @@ module.exports = {
       title: `clearview`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms",
-  {
-    resolve: "gatsby-plugin-manifest",
-    options: {
-      name: "Briefer",
-      short_name: "Briefer",
-      start_url: "/",
-      background_color: "#EBF2FA",
-      theme_color: "#364954",
-      display: "standalone",
-      icon: "static/logo.png"
-    }
-  }
-],
-  
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-react-helmet"
+  ],
 };
