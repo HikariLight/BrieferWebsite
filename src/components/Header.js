ab26f278772d57dcf0678da78111240dd5aaaac9
Dhia M <merzougui.dhia@gmail.com>
2022-04-26 18:34:00 +0200

    Added active nav links css

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ const Header = () =>{
             <Link to="/"><img id="headerLogo" src="logo.png" alt="name"></img></Link>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/features">Features</Link></li>
-                    <li><Link to="/faq">FAQ</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/" activeClassName="activeLink">Home</Link></li>
+                    <li><Link to="/features" activeClassName="activeLink">Features</Link></li>
+                    <li><Link to="/faq" activeClassName="activeLink">FAQ</Link></li>
+                    <li><Link to="/contact" activeClassName="activeLink">Contact</Link></li>
+                    <li><Link to="/blog" activeClassName="activeLink">Blog</Link></li>
                 </ul>
             </nav>
         </header>
