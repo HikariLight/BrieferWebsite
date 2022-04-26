df64b4a32045561c3ba3eb2c8320b31897ba122e
Dhia M <merzougui.dhia@gmail.com>
2022-04-26 19:08:19 +0200

    Changed nav links active color

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ const Header = () =>{
             <Link to="/"><img id="headerLogo" src="logo.png" alt="name"></img></Link>
             <nav>
                 <ul>
-                    <li><Link to="/" activeClassName="activeLink">Home</Link></li>
-                    <li><Link to="/features" activeClassName="activeLink">Features</Link></li>
-                    <li><Link to="/faq" activeClassName="activeLink">FAQ</Link></li>
-                    <li><Link to="/contact" activeClassName="activeLink">Contact</Link></li>
-                    <li><Link to="/blog" activeClassName="activeLink">Blog</Link></li>
+                    <li><Link to="/" activeClassName="activeNavLink">Home</Link></li>
+                    <li><Link to="/features" activeClassName="activeNavLink">Features</Link></li>
+                    <li><Link to="/faq" activeClassName="activeNavLink">FAQ</Link></li>
+                    <li><Link to="/contact" activeClassName="activeNavLink">Contact</Link></li>
+                    <li><Link to="/blog" activeClassName="activeNavLink">Blog</Link></li>
                 </ul>
             </nav>
         </header>
