7c9a0b96a209842750eca22d17b1ca92cfdc50ff
Dhia M <merzougui.dhia@gmail.com>
2022-04-27 19:30:36 +0200

    Fixed logo and active nav color bugs for blog posts pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import "../style/global.css"
 const Header = () =>{
     return(
         <header>
-            <Link to="/"><img id="headerLogo" src="logo.png" alt="name"></img></Link>
+            <Link to="/"><img id="headerLogo" src="/logo.png" alt="name"></img></Link>
             <nav>
                 <ul>
                     <li><Link to="/" activeClassName="activeNavLink">Home</Link></li>
                     <li><Link to="/features" activeClassName="activeNavLink">Features</Link></li>
                     <li><Link to="/faq" activeClassName="activeNavLink">FAQ</Link></li>
                     <li><Link to="/contact" activeClassName="activeNavLink">Contact</Link></li>
-                    <li><Link to="/blog" activeClassName="activeNavLink">Blog</Link></li>
+                    <li><Link to="/blog" activeClassName="activeNavLink" partiallyActive="activeNavLink">Blog</Link></li>
                 </ul>
             </nav>
         </header>
