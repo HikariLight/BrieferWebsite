f93d1d65d887bc30c5649b0f22816d0c4b3450af
Dhia M <merzougui.dhia@gmail.com>
2022-04-27 19:18:52 +0200

    Refactored blog post css

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown'
 import Layout from '../components/Layout';
+import * as postStyle from "../style/BlogPost.module.css"
 
 const Post = (props) =>{
     return(
         <Layout>
-            <main className='blogPostMain'>
+            <main className = {postStyle.blogPostMain}>
                 <h1>{props.pageContext.title}</h1>
-                <ReactMarkdown>
+                <ReactMarkdown className = {postStyle.blogPostBody}>
                     {props.pageContext.body.data.body}
                 </ReactMarkdown>
             </main>
